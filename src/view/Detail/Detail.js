377db39f48bdc8382073dfe33fd60ea2d920aba0
maliaoMJ <1302151931@qq.com>
2018-01-14 15:53:29 +0800

    detail content

--- src/view/Detail/Detail.js
+++ src/view/Detail/Detail.js
@@ -1,15 +1,45 @@
 import React ,{Component} from 'react'
 import TitleHeader from '../../components/TitleHeader/TitleHeader'
+import Loadding from '../../components/Loadding/Loadding'
 import getData from '../../util/getData'
+import 'github-markdown-css'
 import './Detail.less'
 
 class Detail extends Component{
-
+    
+    constructor(props){
+        super(props)
+        this.state = {
+            detailContent:{},
+            user:{}
+        }
+    }
 
     render(){
         return (
             <div className="Detail">
-                <TitleHeader title={'曾经有一份真挚的爱情摆在我的面前，我却没有好好珍惜，如果上天能给我一次从来的机会，我会那个妹子说三个字：给我滚'} history={this.props.history}/>
+                
+                <TitleHeader title={this.state.detailContent.title} history={this.props.history}/>
+                {this.state.detailContent.author == null||undefined?<Loadding/>:(
+                    <div className="user_header ">
+                        <img src={this.state.user.avatar_url} alt="没有见过这么帅的图像吗？" />
+                        <div className="user_info clearfix">
+                            <span className="nickname">{this.state.user.loginname}</span><span className="time">1小时前</span>
+                            <span className="floor_faster">#楼主</span>
+                        </div>
+                    </div>
+                )}
+                
+                <div className="detail_content">
+                    <h3 className="title">{this.state.detailContent.title}</h3>
+                    <div className="tip_icon_box">
+                        <div className="icon_item"><i className="fa fa-eye"></i>&nbsp;{this.state.detailContent.visit_count}</div>
+                        <div className="icon_item"><i className="fa fa-comments"></i>&nbsp;{this.state.detailContent.reply_count}</div>
+                    </div>
+                    <div className="content_html" dangerouslySetInnerHTML={{ __html: this.state.detailContent.content }}>
+                        {/* 文章内容 */}
+                    </div>
+                </div>
             </div>
         )
     }
@@ -21,6 +51,10 @@ class Detail extends Component{
         .then(data=>data.json())
         .then(json=>{
             console.log(json)
+            this.setState({
+                detailContent:json.data,
+                user:json.data.author
+            })
         })
         .catch(error=>{
             console.log(error)
